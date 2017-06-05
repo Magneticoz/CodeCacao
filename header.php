5a3c101847a58ef97e62f62e8a4bc6936e592918
Mishell Novosel <mishell.novosel@hotmail.com>
2017-06-05 17:09:03 +0200

    header changes
    changing meta viewport and adding title

--- header.php
+++ header.php
@@ -10,9 +10,10 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<title>CodeCacao</title>
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
